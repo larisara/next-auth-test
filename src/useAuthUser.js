adcc2d871a3a1046fb17d23af10046d041909a74
laris <larisara@gmail.com>
2022-01-02 19:42:41 +0700

    remove unused files

--- src/useAuthUser.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-import createAuthUser from 'src/createAuthUser'
-
-// Defaults to empty AuthUser object.
-export const AuthUserContext = createContext(createAuthUser())
-
-const useAuthUser = () => useContext(AuthUserContext)
-
-export default useAuthUser
